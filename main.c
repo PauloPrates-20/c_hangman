e239f735f236f7b947e6b8eb15f0a79f34c71e09
Paulo Prates <paulo.prates01@hotmail.com>
2026-07-23 12:35:58 -0300

    Create frame

--- main.c
+++ main.c
@@ -2,6 +2,17 @@
 #include "config.h"
 #include "keyboard.h"
 
+static void DrawFrame() {
+    Rectangle rec = {
+        .x = OFFSET - FR_THICK*2,
+        .y = OFFSET - FR_THICK*2,
+        .width = CELL_SIZE*CELL_COUNT - OFFSET/2 - FR_THICK*2,
+        .height = CELL_SIZE*CELL_COUNT - OFFSET/2 - FR_THICK*2,
+    };
+
+    DrawRectangleLinesEx(rec, FR_THICK, SPRITE_GREEN);
+}
+
 int main(void) {
     InitWindow(CELL_SIZE*CELL_COUNT + OFFSET, CELL_SIZE*CELL_COUNT + OFFSET, "Retro Hangman");
     SetTargetFPS(60);
@@ -12,6 +23,7 @@ int main(void) {
         BeginDrawing();
 
         ClearBackground(BG_GREEN);
+        DrawFrame();
         board.Draw(&board);
 
         EndDrawing();
